990360e301073df065b4a0e709cd91b1e20a4202
Mateus Souza <mateusdesouzapaula97@gmail.com>
2020-11-24 00:18:18 -0300

    add: operation of the button

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
-import "./App.css";
+import React, { useState } from "react";
+import styled from "styled-components";
+import { Modal } from "./components/Modal/Modal";
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`;
+const Button = styled.button`
+  min-width: 100px;
+  padding: 16px 32px;
+  border-radius: 4px;
+  border: none;
+  background: #141414;
+  color: #fff;
+  font-size: 24px;
+  cursor: pointer;
+`;
 
 function App() {
+  const [showModal, setShowModal] = useState(false);
+  const openModal = () => {
+    setShowModal((previous) => !previous);
+  };
+
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
+    <>
+      <Container>
+        <Button onClick={openModal}>I'm a modal</Button>
+        <Modal showModal={showModal} setShowModal={setShowModal} />
+      </Container>
+    </>
   );
 }
 
---
+++ src/components/Modal/Modal.js
@@ -0,0 +1,3 @@
+export const Modal = ({ showModal, setShowModal }) => {
+  return <>{showModal ? <div>Modal</div> : null}</>;
+};
