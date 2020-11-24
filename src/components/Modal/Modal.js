8fe27efc8913e313669b1dea788326d3b00d886d
Mateus Souza <mateusdesouzapaula97@gmail.com>
2020-11-24 11:29:13 -0300

    add: Modal Elements

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,3 +1,88 @@
+import styled from "styled-components";
+import { MdClose } from "react-icons/md";
+
+const Background = styled.div`
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.8);
+  position: fixed;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+const ModalWrapper = styled.div`
+  width: 800px;
+  height: 500px;
+  box-shadow: 0 5px 16px rgba(0, 0, 0, 0.2);
+  background: #fff;
+  color: #000;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  position: relative;
+  z-index: 10;
+  border-radius: 10px;
+`;
+
+const ModalImg = styled.img`
+  width: 100%;
+  height: 100%;
+  border-radius: 10px 0 0 10px;
+  background: #000;
+`;
+
+const ModalContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  line-height: 1.8;
+  color: #141414;
+
+  p {
+    margin-bottom: 1rem;
+  }
+
+  button {
+    padding: 10px 24px;
+    background: #141414;
+    color: #fff;
+    border: none;
+  }
+`;
+
+const CloseModalButton = styled(MdClose)`
+  cursor: pointer;
+  position: absolute;
+  top: 20px;
+  right: 20px;
+  width: 32px;
+  height: 32px;
+  padding: 0;
+  z-index: 10;
+`;
+
 export const Modal = ({ showModal, setShowModal }) => {
-  return <>{showModal ? <div>Modal</div> : null}</>;
+  return (
+    <>
+      {showModal ? (
+        <Background>
+          <ModalWrapper showModal={showModal}>
+            <ModalImg
+              src={require("../../image/modal.jpg")}
+              alt="Fitness Boy"
+            />
+            <ModalContent>
+              <h1>Are you ready?</h1>
+              <p>Get exclusive access to our next launch.</p>
+              <button>Join Now</button>
+            </ModalContent>
+            <CloseModalButton
+              aria-label="Close Modal"
+              onClick={() => setShowModal((previous) => !previous)}
+            />
+          </ModalWrapper>
+        </Background>
+      ) : null}
+    </>
+  );
 };
