b23df0b89b2d54fca819b9fe39ef5d44d8c3b54e
Mateus Souza <mateusdesouzapaula97@gmail.com>
2020-12-03 23:04:07 -0300

    upgrade: applying responsivity on Modal

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import "./App.css";
 import styled from "styled-components";
 import { Modal } from "./components/Modal/Modal";
 import { GlobalStyle } from "./globalStyles";
--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -2,57 +2,93 @@ import React, { useRef, useEffect, useCallback } from "react";
 import { useSpring, animated } from "react-spring";
 import styled from "styled-components";
 import { MdClose } from "react-icons/md";
-import Fitness from "../../image/modal.jpg";
+import modalImage from "../../image/modal.jpg";
 
 const Background = styled.div`
   width: 100%;
   height: 100%;
-  background: rgba(0, 0, 0, 0.8);
+  top: 0;
+  right: 0;
+  left: 0;
+  bottom: 0;
+  background: rgba(0, 0, 0, 0.9);
   position: fixed;
   display: flex;
   justify-content: center;
   align-items: center;
+  padding: 0 1rem;
 `;
 
 const ModalWrapper = styled.div`
-  width: 900px;
-  height: 600px;
-  box-shadow: 0 5px 16px rgba(0, 0, 0, 0.2);
+  width: 100%;
+  height: 100%;
+  max-width: 900px;
+  margin: 2rem auto 0 auto;
+  box-shadow: 3px 3px 6px rgba(0, 0, 0, 0.2);
   background: #fff;
-  color: #000;
   display: grid;
   grid-template-columns: 1fr 1fr;
   position: relative;
-  z-index: 10;
+  z-index: 999;
   border-radius: 10px;
+
+  @media screen and (max-width: 676px) {
+    grid-template-columns: 1fr;
+    background: linear-gradient(
+        to right,
+        rgba(0, 0, 0, 0.7),
+        rgba(0, 0, 0, 0.7)
+      ),
+      url(${modalImage});
+    background-position: center;
+    background-size: cover;
+    color: #ffffff;
+  }
 `;
 
 const ModalImg = styled.img`
   width: 100%;
-  height: 100%;
-  border-radius: 9px 0 0 9px;
-  background: #000;
+  max-height: 675px;
+  border-radius: 7px 0 0 7px;
+  @media screen and (max-width: 676px) {
+    display: none;
+  }
 `;
 
 const ModalContent = styled.div`
   display: flex;
   flex-direction: column;
-  justify-content: center;
+  justify-content: space-evenly;
   align-items: center;
-  text-align: start;
-  line-height: 1.8;
-  color: #141414;
+  text-align: center;
+  padding: 2rem;
 
   h1 {
-    margin-bottom: 3rem;
-  }
-
-  h3 {
+    font-size: clamp(1.5rem, 2vw, 2.25rem);
     margin-bottom: 1rem;
   }
 
+  h3 {
+    font-size: clamp(1.25rem, 1.75vw, 1.75rem);
+    margin: 1rem 0;
+  }
+
   p {
-    margin-bottom: 2rem;
+    font-size: clamp(0.875rem, 1vw, 1.25rem);
+    max-width: 500px;
+    padding: 0 0.75rem;
+
+    @media screen and (max-width: 676px) {
+      padding: 0 0.25rem;
+    }
+  }
+
+  li {
+    text-align: start;
+  }
+
+  li + li {
+    margin-top: 0.25rem;
   }
 
   button {
@@ -62,6 +98,11 @@ const ModalContent = styled.div`
     border: none;
     cursor: pointer;
     margin-top: 3rem;
+
+    @media screen and (max-width: 676px) {
+      background: #fff;
+      color: #141414;
+    }
   }
 `;
 
@@ -112,7 +153,7 @@ export const Modal = ({ showModal, setShowModal }) => {
         <Background ref={modalRef} onClick={closeModal}>
           <animated.div style={animation}>
             <ModalWrapper showModal={showModal}>
-              <ModalImg src={Fitness} alt="Fitness Boy" />
+              <ModalImg src={modalImage} alt="Fitness Boy" />
               <ModalContent>
                 <h1>Hipertrofia</h1>
                 <h3>O que será feito?</h3>
@@ -123,9 +164,8 @@ export const Modal = ({ showModal, setShowModal }) => {
                 <h3>Iremos analisar:</h3>
                 <ul>
                   <li>Biotipo corporal</li>
-                  <li>
-                    Análise da taxa de metabolismo basal e cálculo de calorias
-                  </li>
+                  <li>Análise da taxa de metabolismo basal</li>
+                  <li>Análise de cálculo de calorias</li>
                   <li>Adequação da dieta de acordo com o treino</li>
                 </ul>
                 <button>Entre em Contato</button>
